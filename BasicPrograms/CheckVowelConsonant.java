4f1f56ad22ba51eb522cb5c7029cbb8262a274d9
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-07-10 20:57:11 +0530

    Add files via upload

---
+++ BasicPrograms/CheckVowelConsonant.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class CheckVowelConsonant {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        String word = scan.nextLine();
+        if (word.length() == 1) {
+            if (word.equals("A") || word.equals("a") || word.equals("E") || word.equals("e") || word.equals("I") || word.equals("i") || word.equals("O") || word.equals("o") || word.equals("U") || word.equals("u")) {
+                    System.out.print("It is a vowel");
+            }
+            else {
+                System.out.print("It is a consonant");
+            }
+        }
+        else {
+            System.out.print("Enter a single character");
+        }
+
+    }
+    
+}
