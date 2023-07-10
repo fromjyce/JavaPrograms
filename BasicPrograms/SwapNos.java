1e06f87f8ed07e0ff2c37c95f0b2f91c839a3114
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-07-10 19:12:50 +0530

    Add files via upload

---
+++ BasicPrograms/SwapNos.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SwapNos {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int temp;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        a = scan.nextInt();
+        System.out.print("\nEnter the second number: ");
+        b = scan.nextInt();
+        System.out.println("\nFirst number is " + a);
+        System.out.println("Second number is " + b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("\nFirst Number is : "+ a);
+        System.out.println("Second Number is : " + b);
+    }
+    
+}
