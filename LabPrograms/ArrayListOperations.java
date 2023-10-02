520ec2f763f4b47d00293296642b12de8f968a38
jaya2 <jaya2004kra@gmail.com>
2023-10-02 13:52:54 +0530

    finalized

--- LabPrograms/ArrayListOperations.java
+++ LabPrograms/ArrayListOperations.java
@@ -16,7 +16,7 @@ public class ArrayListOperations {
     void addingelements() {
         int choice = 0;
         while (choice != 4) {
-            System.out.print("1. Insert At Beginning \n2. Insert At A Position \n3. Insert at End \nEnter Your Choice: ");
+            System.out.print("\n1. Insert At Beginning \n2. Insert At A Position \n3. Insert at End \n4. EXIT \nEnter Your Choice: ");
             choice = scan.nextInt();
             scan.nextLine();
             if (choice==1) {
@@ -25,7 +25,7 @@ public class ArrayListOperations {
                 Elements.add(0, elem);
                 break;
             } else if (choice==2) {
-                System.out.print("Enter the Position" );
+                System.out.print("Enter the Position: " );
                 int pos = scan.nextInt();
                 scan.nextLine();
                 System.out.print("Enter the Element: ");
@@ -35,7 +35,9 @@ public class ArrayListOperations {
             } else if (choice==3) {
                 System.out.print("Enter the Element: ");
                 String elem = scan.nextLine();
-                Elements.add((Elements.size()-1), elem);
+                Elements.add((Elements.size()), elem);
+                break;
+            } else {
                 break;
             }
         }
@@ -46,29 +48,56 @@ public class ArrayListOperations {
         System.out.print("Enter Position: ");
         int pos = scan.nextInt();
         scan.nextLine();
-        Elements.remove(pos);
+        Elements.remove(pos-1);
     }
 
     void searchingelements() {
         System.out.print("Enter the Element that you want to search: ");
         String elem = scan.nextLine();
         if (Elements.contains(elem) == true) {
-            System.out.print("Element found at" + (Elements.indexOf(elem)+1));
+            System.out.print("Element found at " +(Elements.indexOf(elem)+1));
         }
         else {
             System.out.print("Element not found");
+            
         }
     }
 
     void listingelements() {
+        int choice = 0;
+        while (choice !=3) {
+            System.out.print("1. List All Elements \n2. List All Elements that starts with a character \n3. EXIT \nEnter Your Choice: ");
+            choice = scan.nextInt();
+            scan.nextLine();
+            if (choice == 1) {
+                for (String elem: Elements) {
+                    System.out.println(elem);
+                    
+                }
+                break;
+            }
+            else if (choice==2) {
+                listingelementswithletters();
+                break;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    void listingelementswithletters() {
+        ArrayList<String> FoundElem = new ArrayList<String>();
         System.out.print("Enter the Character: ");
         char eleml = scan.next().charAt(0);
         for (int i = 0; i<Elements.size(); i++) {
             if (Elements.get(i).charAt(0) == eleml) {
-                System.out.print("Element found - " + Elements.get(i));
-            } else {
-                System.out.print("Element not found");
+                FoundElem.add(Elements.get(i));
+                
             }
+        }
+        for (String elem: FoundElem) {
+            System.out.println(elem);
         }
     }
     public static void main(String[] args) {
@@ -76,7 +105,7 @@ public class ArrayListOperations {
         try {
             int mainchoice = 0;
             while (mainchoice!=5) {
-                System.out.print("Do you want to \n1. Add Element \n2. Delete Element \n3. Search Element \n4. List All Elements \n5. Exit \nEnter Your Choice: ");
+                System.out.print("\nDo you want to \n1. Add Element \n2. Delete Element \n3. Search Element \n4. List All Elements \n5. Exit \nEnter Your Choice: ");
                 mainchoice = arrlist.scan.nextInt();
                 arrlist.scan.nextLine();
                 if (mainchoice==1) {
@@ -96,7 +125,7 @@ public class ArrayListOperations {
                     arrlist.listingelements();
                 }
 
-                else if (mainchoice==5) {
+                else{
                     break;
                 }
             }
