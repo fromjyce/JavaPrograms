48ae0e908249a9680f704b0adc69601d3792b81f
jaya2 <jaya2004kra@gmail.com>
2023-10-02 14:18:01 +0530

    added week 6 file

---
+++ LabPrograms/UserDefinedExceptionHandling.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+
+public class UserDefinedExceptionHandling {
+
+    static void validate(int age) throws InValidAgeError {
+        if (age <= 18) {
+            throw new InValidAgeError("Age is NOT VALID");
+        }
+        else {
+            System.out.println("Congrats!!!You have been validated");
+        }
+    }
+    public static void main(String[] args) {
+        try {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("You have to be more than 18 years old\nEnter Your Age: ");
+        int age = scan.nextInt();
+        scan.nextLine();
+        validate(age);
+        } catch (InValidAgeError ex) {
+            System.out.println("Exception Occured: " + ex);
+        }
+        
+}
+}
+
+class InValidAgeError extends Exception {
+    public InValidAgeError(String errorMessage) {
+        super(errorMessage);
+    }
+}
