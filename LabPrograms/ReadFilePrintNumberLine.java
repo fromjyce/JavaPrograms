94fb908c98f9b9f7d654be6ba3cea1fdf8daf3db
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-10-31 11:11:19 +0530

    Update ReadFilePrintNumberLine.java

--- LabPrograms/ReadFilePrintNumberLine.java
+++ LabPrograms/ReadFilePrintNumberLine.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.io.*;
 
-public class ProblemOne {
+public class ReadFilePrintNumberLine {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the filename with extension: ");
