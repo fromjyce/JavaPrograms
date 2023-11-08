da126b85ba40f101d19b1acf0efd4e72fc5228b8
jaya2 <jaya2004kra@gmail.com>
2023-11-08 21:54:40 +0530

    modified program

--- LabPrograms/FileEmptyChecker.java
+++ LabPrograms/FileEmptyChecker.java
@@ -1,20 +1,35 @@
 /*File Handling: Check if a File Exists and if It's Empty, Throw an IOException */
 
 import java.io.File;
-import java.io.IOException;
+
 
 public class FileEmptyChecker {
     public static void main(String[] args) {
         File file = new File("yourfile.txt");
         
-        if (file.exists()) {
-            if (file.length() == 0) {
-                System.out.println("File is empty.");
-            } else {
-                System.out.println("File exists and is not empty.");
-            }
+       try {
+        EmptyChecker(file);
+        System.out.println("File is not empty.");
+       } catch (FileEmptyWarning e) {
+        System.err.println("File is empty or does not exist.");
+       }
+    }
+
+    public static void EmptyChecker (File Filename) throws FileEmptyWarning {
+        if (Filename.exists()) {
+            if (Filename.length() == 0 ) {
+                throw new FileEmptyWarning("File is Empty");
+            } 
         } else {
-            System.out.println("File does not exist.");
+            throw new FileEmptyWarning("File does not exist");
         }
+
+    }
+}
+
+class FileEmptyWarning extends Exception {
+
+public FileEmptyWarning(String errorMessage) {
+        super(errorMessage);
     }
 }
