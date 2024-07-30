0258483f03e3663861956257c5c461f09c1ab16d
jaya2 <jaya2004kra@gmail.com>
2024-07-30 15:00:33 +0530

    added pp program

---
+++ OtherPrograms/Reverse.java
@@ -0,0 +1,15 @@
+/*Hello World -> World Hello */
+
+import java.util.Scanner;
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String string = scan.nextLine();
+
+        String[] arr = string.split(" ");
+
+        for(int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
