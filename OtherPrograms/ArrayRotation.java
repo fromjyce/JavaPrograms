62947dd4989d2036104b9e30e35e5c6988e4e45f
jaya2 <jaya2004kra@gmail.com>
2024-07-30 14:29:13 +0530

    added other program

---
+++ OtherPrograms/ArrayRotation.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class ArrayRotation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int[][] mat = new int[num][num];
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                mat[i][j] = scan.nextInt();
+            }
+        }
+
+        for (int i = 0; i < num/2; i++) {
+            for (int j = i; j < num - i - 1; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[num - 1 - j][i];
+                mat[num - 1 - j][i] = mat[num - 1 - i][num - 1 - j];
+                mat[num - 1 - i][num - 1 - j] = mat[j][num - 1 - i];
+                mat[j][num - 1 - i] = temp;
+            }
+        }
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                System.out.print(mat[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
