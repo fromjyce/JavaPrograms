df38807fea1fa0dc55e4b485bb5ba381889b829a
jaya2 <jaya2004kra@gmail.com>
2024-08-02 19:37:44 +0530

    added 2nd program of assessment

---
+++ DSAPrograms/BalancedParanthesis.java
@@ -0,0 +1,40 @@
+package DSAPrograms;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalancedParanthesis {
+    public static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (!isMatchingPair(top, ch)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static boolean isMatchingPair(char open, char close) {
+        return (open == '(' && close == ')') ||
+               (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (isBalanced(input)) {
+            System.out.println("balanced");
+        } else {
+            System.out.println("not Balanced");
+        }
+    }
+}
