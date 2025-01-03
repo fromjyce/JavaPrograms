da79d9a396881639756ceef8d16982ff22d317c1
Jayashre <jaya2004kra@gmail.com>
2025-01-03 22:57:26 +0530

    added 1st jan first program

---
+++ LabPrograms/PasswordChecker.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class PasswordValidator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a password: ");
+        String password = scanner.nextLine();
+
+        if (isPerfectPassword(password)) {
+            System.out.println("Perfect Password");
+        } else {
+            System.out.println("Not a Perfect Password");
+        }
+    }
+
+    public static boolean isPerfectPassword(String password) {
+        if (password.length() != 9) {
+            return false;
+        }
+
+        boolean hasLower = false;
+        boolean hasUpper = false;
+        boolean hasNumber = false;
+        boolean hasSpecial = false;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                hasLower = true;
+            } else if (Character.isUpperCase(c)) {
+                hasUpper = true;
+            } else if (Character.isDigit(c)) {
+                hasNumber = true;
+            } else if (!Character.isWhitespace(c) && !Character.isLetterOrDigit(c)) {
+                hasSpecial = true;
+            }
+        }
+
+        return hasLower && hasUpper && hasNumber && hasSpecial;
+    }
+}
