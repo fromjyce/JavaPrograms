f460a5448da203dafdb2f76361d3abba3e248086
Jayashre <jaya2004kra@gmail.com>
2025-01-04 08:49:49 +0530

    added code

--- OtherPrograms/FactorialSolver.java
+++ OtherPrograms/FactorialSolver.java
@@ -5,13 +5,33 @@ public class FactorialSolver {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
-        
-        long factorial = 1;
-        for (int i = 1; i <= number; i++) {
-            factorial *= i;
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else if (number == 0) {
+            System.out.println("Factorial of 0 is 1. Yes");
+        } else {
+            long factorial = 1;
+            for (int i = 1; i <= number; i++) {
+                factorial *= i;
+            }
+            System.out.println("Factorial of " + number + " is: " + factorial);
+            if (containsDigitOne(factorial)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
         }
-        
-        System.out.println("Factorial of " + number + " is: " + factorial);
+
         scanner.close();
+    }
+
+    public static boolean containsDigitOne(long number) {
+        while (number > 0) {
+            if (number % 10 == 1) {
+                return true;
+            }
+            number /= 10;
+        }
+        return false;
     }
 }
