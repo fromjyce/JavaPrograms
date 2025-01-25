6ac2c847974dec50c54ca9b9a7e02a2a5115d72f
fromjyce <jaya2004kra@gmail.com>
2025-01-25 14:09:49 +0530

    added placement prog

--- OtherPrograms/Hackthemoney.java
+++ OtherPrograms/Hackthemoney.java
@@ -1,5 +1,6 @@
-/*Initially 1 Ruppee in your account. YOu want exactly N Ruppees in your account. You wrote two hacks - First: Can Multiply the amount of money you own by 10. Second: Can multiply the amount by 20. These hacks can be used any number of tuime. Can you achieve the desired amount N using these hacks. */
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Hackthemoney {
     public static void main(String[] args) {
@@ -14,21 +15,31 @@ public class Hackthemoney {
             System.out.println("No");
         }
     }
+
     public static boolean canReachAmount(long N) {
-        if (N < 1) {
+        Set<Long> visited = new HashSet<>();
+        return driver(N, visited);
+    }
+
+    private static boolean driver(long N, Set<Long> visited) {
+        if (N == 1) {
+            return true;
+        }
+
+        if (N < 1 || visited.contains(N)) {
             return false;
         }
 
-        while (N > 1) {
-            if (N % 10 == 0) {
-                N /= 10;
-            } else if (N % 20 == 0) {
-                N /= 20;
-            } else {
-                return false;
-            }
+        visited.add(N);
+
+        if (N % 20 == 0 && driver(N / 20, visited)) {
+            return true;
         }
 
-        return N == 1;
+        if ( N % 10 == 0 && driver(N / 10, visited)) {
+            return true;
+        }
+
+        return false;
     }
 }
