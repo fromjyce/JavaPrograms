a6823aefdec670d4383555ce34eb759f736eba04
fromjyce <jaya2004kra@gmail.com>
2025-02-22 15:08:29 +0530

    added reverse task

--- OtherPrograms/ReverseStringPreserveSpaces.java
+++ OtherPrograms/ReverseStringPreserveSpaces.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 
@@ -5,7 +6,19 @@ public class ReverseStringPreserveSpaces {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String input= scan.nextLine();
+        System.out.println(getCharacterFrequency(input.toLowerCase().replaceAll("\\s+","")));
         System.out.println(reversePreserveSpaces(input));
+        scan.close();
+    }
+
+    public static HashMap<Character, Integer> getCharacterFrequency(String str) {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        
+        for (char c : str.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+        
+        return freqMap;
     }
 
     public static String reversePreserveSpaces(String input) {
